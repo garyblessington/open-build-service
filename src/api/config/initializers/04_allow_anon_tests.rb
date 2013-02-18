abb2c38c3d92eac1e650ea74eda941d0f19032f7
Gary Blessington <garyblessington@gmail.com>
2013-02-18 11:58:40 -0800

    [api] Reverted how CONFIG[:allow_anonymous] is initialzed for test

--- src/api/config/initializers/04_allow_anon_tests.rb
+++ src/api/config/initializers/04_allow_anon_tests.rb
@@ -1,4 +1,4 @@
-# if Rails.env.test?
-#   CONFIG['allow_anonymous'] = true
-#   CONFIG['read_only_hosts'] = ['127.0.0.1', '::1']
-# end
+if Rails.env.test?
+  CONFIG['allow_anonymous'] = true
+  CONFIG['read_only_hosts'] = ['127.0.0.1', '::1']
+end
